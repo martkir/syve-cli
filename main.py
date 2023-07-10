cd435728f663019ce721c01a369fd8cbcaf52095
martin <martinbarut@hotmail.com>
2023-07-10 14:43:29 +0200

    added size param

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_last_timestamp(save_dir):
             return last_timestamp
 
 
-def download(slug, from_timestamp, until_timestamp, data_dir, size=5001):
+def download(slug, from_timestamp, until_timestamp, size, data_dir):
     save_dir = f"{data_dir}/{slug}"
     last_timestamp = get_last_timestamp(save_dir)
     if last_timestamp is not None:
@@ -104,13 +104,19 @@ def download(slug, from_timestamp, until_timestamp, data_dir, size=5001):
     help="The time to stop downloading at in UNIX timestamp seconds. Defaults to now.",
 )
 @click.option(
+    "--size",
+    type=int,
+    default=10000,
+    help="The number of records to download per request. Defaults to 10,000.",
+)
+@click.option(
     "--data-dir",
     type=str,
     default=f"{os.getcwd()}/data",
     help="The directory where data is stored. Defaults to ./data.",
 )
-def main(slug, from_timestamp, until_timestamp, data_dir):
-    download(slug, from_timestamp, until_timestamp, data_dir)
+def main(slug, from_timestamp, until_timestamp, size, data_dir):
+    download(slug, from_timestamp, until_timestamp, size, data_dir)
 
 
 if __name__ == "__main__":
